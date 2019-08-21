e6479ad0c4e5afa26a9d7c4f151790fafdfdf4c1
NicoRenaud <nicolas.gm.renaud@gmail.com>
2019-08-21 09:44:43 +0200

    refactor

--- setup.py
+++ setup.py
@@ -1,10 +1,49 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+import os
+
+from setuptools import (find_packages, setup)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# To update the package version number, edit DeepQMC/__version__.py
+version = {}
+with open(os.path.join(here, 'pdb2sql', '__version__.py')) as f:
+    exec(f.read(), version)
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
 setup(
     name='pdb2sql',
-    description='Using SQL queries to manipulate PDB files',
-    version='0.1-dev0',
-    url=' ',
-    packages=['pdb2sql'],
-    install_requires=['numpy']
+    version=version['__version__'],
+    description="PDB parser using SQL queries",
+    long_description=readme + '\n\n',
+    long_description_content_type='text/markdown',
+    author=["Nicolas Renaud"],
+    author_email='n.renaud@esciencecenter.nl',
+    url='https://github.com/DeepRank/pdb2sql',
+    packages=find_packages(),
+    package_dir={'pdb2sql': 'pdb2sql'},
+    include_package_data=True,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='PDB2SQL',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'intended audience :: science/research',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: BioChemistry'
+    ],
+    test_suite='tests',
+    install_requires=['cython', 'sqlite', 'sqlalchemy', 'matplotlib', 'numpy', 'pyyaml>=5.1',
+					  'schema',  'tqdm'],
+    extras_require={
+        'dev': ['prospector[with_pyroma]', 'yapf', 'isort'],
+        'doc': ['recommonmark', 'sphinx', 'sphinx_rtd_theme'],
+        'test': ['coverage', 'pycodestyle', 'pytest', 'pytest-cov', 'pytest-runner'],
+    }
 )
