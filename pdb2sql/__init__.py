ed1fdeda54c1e5a554e482ac2916f98e1a3e5e6c
Cunliang Geng <c.geng@esciencecenter.nl>
2020-02-28 16:18:31 +0100

    Add utils fetch method to download pdb file

--- pdb2sql/__init__.py
+++ pdb2sql/__init__.py
@@ -2,5 +2,6 @@ from .pdb2sqlcore import pdb2sql
 from .interface import interface
 from .StructureSimilarity import StructureSimilarity
 from . import transform
+from .utils import fetch
 
 from .__version__ import __version__
