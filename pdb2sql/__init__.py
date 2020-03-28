7dd1d867127700bd18b460d9baadb0dc86a19e12
Cunliang Geng <c.geng@esciencecenter.nl>
2020-03-28 12:33:14 +0100

    Update __init__.py

--- pdb2sql/__init__.py
+++ pdb2sql/__init__.py
@@ -3,5 +3,7 @@ from .interface import interface
 from .StructureSimilarity import StructureSimilarity
 from . import transform
 from .utils import fetch
+from .align import align, align_interface
+from .superpose import superpose
 
 from .__version__ import __version__
