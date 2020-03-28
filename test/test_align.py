7caefc3da63e41dc83414d0258f3b74260cdd07d
Cunliang Geng <c.geng@esciencecenter.nl>
2020-03-28 12:07:14 +0100

    update warnings and errors

--- test/test_align.py
+++ test/test_align.py
@@ -10,27 +10,31 @@ class TestAlign(unittest.TestCase):
         self.pdb = 'pdb/1AK4/1AK4_10w.pdb'
 
     def test_align(self):
-        """test the routine."""
+        """Test align()"""
 
         for idir, axis in zip([0, 1, 2], ['x', 'y', 'z']):
-            sql = align(self.pdb, axis=axis)
-            xyz = np.array(sql.get('x,y,z'))
+            with self.assertWarns(UserWarning) as ex:
+                db = align(self.pdb, axis=axis)
+            xyz = np.array(db.get('x,y,z'))
             u, v = pca(xyz)
             vmax = np.abs(v[:, np.argmax(u)])
             assert np.argmax(vmax) == idir
 
     def test_align_interface(self):
-        """test the routine."""
+        """Test align_interface()"""
 
-        def get_xyz_interface(sql):
-            idx = sql.get_contact_atoms()
+        def get_xyz_interface(db):
+            idx = db.get_contact_atoms()
             idx = idx['A'] + idx['B']
-            return np.array(sql.get('x,y,z', rowID=idx)) 
+            return np.array(db.get('x,y,z', rowID=idx))
 
         for idir, plane in zip([2, 1, 0], ['xy', 'xz', 'yz']):
-            sql = align_interface(self.pdb, plane=plane)
-            xyz = get_xyz_interface(sql)
+            with self.assertWarns(UserWarning) as ex:
+                db = align_interface(self.pdb, plane=plane)
+            xyz = get_xyz_interface(db)
             u, v = pca(xyz)
             vmax = np.abs(v[:, np.argmin(u)])
             assert np.argmax(vmax) == idir
 
+if __name__ == '__main__':
+    unittest.main()
--- test/test_superpose.py
+++ test/test_superpose.py
@@ -7,14 +7,22 @@ class TestSuperpose(unittest.TestCase):
     """Test the superpose functionality"""
 
     def setUp(self):
-
         self.pdb1 = 'pdb/1AK4/1AK4_5w.pdb'
         self.pdb2 = 'pdb/1AK4/1AK4_10w.pdb'
 
     def test_superpose(self):
-        """test the routine."""
-        superpose(self.pdb1, self.pdb2, chainID='A')
+        """Test superpose()"""
+        with self.assertWarns(UserWarning) as ex:
+            superpose(self.pdb1, self.pdb2, chainID='A')
 
-    @unittest.expectedFailure
-    def test_fails(self):
-        superpose(self.pdb1, self.pdb2, name='CA')
+    def test_superpose_backbone_error(self):
+        """Test superpose() backbone error when specifying `name`"""
+        with self.assertWarns(UserWarning) as ex:
+            with self.assertRaises(ValueError) as err:
+                superpose(self.pdb1, self.pdb2, name='CA')
+        err_msg = err.exception.args[0]
+        target = "Atom type specified but only_backbone == True"
+        self.assertIn(target, err_msg)
+
+if __name__ == '__main__':
+    unittest.main()
